a7a1d270249db22dab6990b686f10a7f8f34d669
KarenAedo <karen.aedo.mayorga@gmail.com>
2017-11-12 09:45:20 -0300

    Versión 0.0.1.1-Errores de js arreglados y funcionando

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,26 @@
 
-function add(){
+//moví la función desde el html a js
+botonEnviar.onclick = function(){
 
-    //llamo a mi ID ingreso donde irán los pensamientos
+    //textarea - donde irá el texto que se ingrese
     var ingresoElTexto = document.getElementById("ingresoElTexto").value;
-
-    //Limpio mi cuadro de ingreso
+    //Limpio mi cuadro de ingreso luego de dar click y guardar el texto
     document.getElementById("ingresoElTexto").value = "";
+    //lineaTiempo, div vacío donde luego irán guardados los comentarios
+    var lineaTiempo = document.getElementById("lineaTiempo");
+    
+    //quité el alert porque en realidad no me servía para el ejercicio, estaba demás
 
-    //si ingresa algo vacio retorne un alerta
-    if (ingresoElTexto === ""){
-    alert("Debes ingresar un pensamiento");
-    }
-  
-    else {
+    //creo este div contenedor donde irá inserto el elemento p, para luego poder darle estilo en css
     var nuevosTweets = document.createElement("div"); //hacer nuevo div contenedor
-    nuevosTweets.className = "contenedor" //dando una clase al contenedor
-    var nombreUsuarioSpan = document.createElement("span"); //span con el nombre de usuario
-    nombreUsuarioSpan.className ="usuario"
-    var fechaSpan = document.createElement("span"); //span con fecha de ingreso
-    fechaSpan.className ="fecha"
+    nuevosTweets.setAttribute("id","contenedor"); // dando un id al contenedor
     var nuevoParrafo = document.createElement("p"); //hacer nuevo parrafo
-
-    //agrego los elementos creados(div,span y p) al contenedor
-    nuevosTweets.appendChild(nombreUsuarioSpan);
-    nuevosTweets.appendChild(fechaSpan);
+    nuevoParrafo.setAttribute("id","nuevoParrafo")
+    //agrego el elemento creado al contenedor
     nuevosTweets.appendChild(nuevoParrafo);
 
-    nombreUsuarioSpan.textContent = "Karen Aedo";
-    fechaSpan.textContent = new Date() ;
-    nuevoParrafo.textContent = comment;
-
-
-    var timeLine = document.getElementById("lineaTiempo");
-    timeLine.insertBefore(nuevosTweets, timeLine.children[0]);
-
-   }
- }
+    //enlazo el textarea con el "p" creado, que será quien guarde el texto ingresado
+    nuevoParrafo.textContent = ingresoElTexto;
+    //El método Node.insertBefore() inserta el nuevo elemento antes del elemento de referencia como hijo del nodo actual
+    lineaTiempo.insertBefore(nuevosTweets, lineaTiempo.children[0]);
+}
